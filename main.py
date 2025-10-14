5b89c09701f91ce12820d38dbc96931cf10a5fc0
Maribel Salvador Rufo <maribelsalvadorr@gmail.com>
2025-10-14 18:48:19 +0200

    backend: CRUD completado.

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 import sqlite3
 
+
 class Bd:
-    nombre = "gatos.sqlite3"  
+    nombre = "gatos.sqlite3"
     __conexion = None
+
+    @staticmethod
     def abrir():
         if Bd.__conexion:
             return Bd.__conexion
         else:
             Bd.__conexion = sqlite3.connect(Bd.nombre)
             cursor = Bd.__conexion.cursor()
-            cursor.execute("CREATE TABLE IF NOT EXISTS gatos (id PRIMARY KEY, raza TEXT, descripcion TEXT, ubicacion TEXT, apodo TEXT)")
+            cursor.execute(
+                "CREATE TABLE IF NOT EXISTS gatos (id INTEGER PRIMARY KEY AUTOINCREMENT, raza TEXT, descripcion TEXT, ubicacion TEXT, apodo TEXT)"
+            )
             return Bd.__conexion
 
+    @staticmethod
     def cerrar():
-        Bd.__conexion = Bd.abrir()
-        Bd.__conexion.commit()
-        Bd.__conexion.close()
+        if Bd.__conexion:
+            Bd.__conexion.commit()
+            Bd.__conexion.close()
+            Bd.__conexion = None
 
 
 class Gato:
@@ -30,22 +37,51 @@ def crear_gato(gato):
     conexion = Bd.abrir()
     cursor = conexion.cursor()
     resultado = cursor.execute(
-        f"INSERT INTO gatos (raza, descripcion, ubicacion, apodo) VALUES ('{gato.raza}','{gato.descripcion}', '{gato.ubicacion}', '{gato.apodo}')"
+        "INSERT INTO gatos (raza, descripcion, ubicacion, apodo) VALUES (?, ?, ?, ?)",
+        (gato.raza, gato.descripcion, gato.ubicacion, gato.apodo),
     )
     gato.id = resultado.lastrowid
+
     return gato.id > 0
 
 
 def editar_gato(gato):
-    return False
+    conexion = Bd.abrir()
+    cursor = conexion.cursor()
+    resultado = cursor.execute(
+        "UPDATE gatos SET raza=?, descripcion=?, ubicacion=?, apodo=? WHERE id=?",
+        (gato.raza, gato.descripcion, gato.ubicacion, gato.apodo, gato.id),
+    )
+
+    return resultado.rowcount == 1
 
 
 def borrar_gato(gato):
-    return False
+    conexion = Bd.abrir()
+    cursor = conexion.cursor()
+    resultado = cursor.execute("DELETE FROM gatos WHERE id=?", (gato.id,))
+
+    return resultado.rowcount == 1
 
 
 def listar_gatos():
-    return []
+    conexion = Bd.abrir()
+    cursor = conexion.cursor()
+    resultado = cursor.execute(
+        "SELECT id, raza, descripcion, ubicacion, apodo FROM gatos ORDER BY apodo"
+    )
+    gatos = resultado.fetchall()
+    lista_gatos = []
+    for gato in gatos:
+        instancia = Gato()
+        instancia.id = gato[0]
+        instancia.raza = gato[1]
+        instancia.descripcion = gato[2]
+        instancia.ubicacion = gato[3]
+        instancia.apodo = gato[4]
+        lista_gatos.append(instancia)
+
+    return lista_gatos
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from main import Gato, crear_gato, borrar_gato, Bd
+from main import Gato, crear_gato, borrar_gato, Bd, editar_gato, listar_gatos
 import main
 
 
@@ -11,15 +11,13 @@ def gato_prueba():
     return gato
 
 
-def test_crear_gato():
-    main.nombre_bd = ":memory:"
+def test_crear_gato(conexion):
     gato = gato_prueba()
 
     resultado = crear_gato(gato)
     assert resultado, "crear_gato ha devuelto False."
     assert gato.id > 0, "gato.id no tiene un valor correcto."
 
-    conexion = Bd.abrir()
     cursor = conexion.cursor()
     resultado = cursor.execute(f"SELECT * FROM gatos WHERE id={gato.id}")
     numero_gatos = len(resultado.fetchall())
@@ -27,28 +25,83 @@ def test_crear_gato():
     assert numero_gatos == 1, "numero_gatos debe ser uno."
 
 
-def test_borrar_gato():
-    main.nombre_bd = ":memory:"
+def test_borrar_gato(conexion):
     gato = gato_prueba()
 
     assert crear_gato(gato), "crear_gato no ha funcionado."
     assert borrar_gato(gato), "borrar_gato no ha funcionado."
 
-    conexion = Bd.abrir()
     cursor = conexion.cursor()
-    resultado = cursor.execute(f"SELECT * FROM gatos WHERE id={gato.id}")
-    numero_gatos = len(resultado.fetchall())
+    resultado = cursor.execute(f"SELECT COUNT(*) FROM gatos")
+    numero_gatos = resultado.fetchone()[0]
 
     assert numero_gatos == 0, "numero_gatos debe ser cero."
 
 
+def test_editar_gato(conexion):
+    gato = gato_prueba()
+    assert crear_gato(gato), "crear_gato no ha funcionado."
+
+    gato.apodo = "Colmillitos"
+    assert editar_gato(gato), "editar_gato no ha funcionado."
+
+    cursor = conexion.cursor()
+    resultado = cursor.execute(f"SELECT apodo FROM gatos")
+    gatos = resultado.fetchall()
+
+    assert len(gatos) == 1, "Debe de haber un solo gato."
+    assert (
+        gatos[0][0] == "Colmillitos"
+    ), "El único gato que hay debe llamarse Colmillitos"
+
+
+def test_listar_gatos(conexion):
+    gato1 = gato_prueba()
+    gato2 = gato_prueba()
+    gato3 = gato_prueba()
+
+    gato1.apodo = "Gatito"
+    gato2.apodo = "Pelusa"
+    gato3.apodo = "Pulga"
+
+    assert crear_gato(gato1), "crear_gato para gato1 no ha funcionado."
+    assert crear_gato(gato2), "crear_gato para gato2 no ha funcionado."
+    assert crear_gato(gato3), "crear_gato para gato3 no ha funcionado."
+
+    cursor = conexion.cursor()
+    resultado = cursor.execute(f"SELECT apodo FROM gatos ORDER BY apodo")
+    gatos = resultado.fetchall()
+
+    assert len(gatos) == 3, "Debe de haber 3 gatos en la base de datos."
+    assert gatos[0][0] == "Gatito", "El primer gato debe llamarse Gatito."
+    assert gatos[1][0] == "Pelusa", "El segundo gato debe llamarse Pelusa."
+    assert gatos[2][0] == "Pulga", "El tercer gato debe llamarse Pulga."
+
+    gatos = listar_gatos()
+    assert len(gatos) == 3, "Debe de haber 3 gatos devueltos por listar_gatos."
+    assert isinstance(
+        gatos[0], Gato
+    ), "El primer valor de la lista debe ser de tipo Gato."
+    assert isinstance(
+        gatos[1], Gato
+    ), "El segundo valor de la lista debe ser de tipo Gato."
+    assert isinstance(
+        gatos[2], Gato
+    ), "El tercer valor de la lista debe ser de tipo Gato."
+
+    assert gatos[0].apodo == "Gatito", "El primer gato debe apodarse Gatito."
+    assert gatos[1].apodo == "Pelusa", "El segundo gato debe apodarse Pelusa."
+    assert gatos[2].apodo == "Pulga", "El tercer gato debe apodarse Pulga."
+
+
 if __name__ == "__main__":
-    try:
-        for fn in [test_crear_gato, test_borrar_gato]:
-            try:
-                fn()
-                print(f"✅ {fn.__name__}")
-            except AssertionError as e:
-                print(f"❌ {fn.__name__}: {e}")
-    finally:
-        Bd.cerrar()
+    for fn in [test_crear_gato, test_borrar_gato, test_editar_gato, test_listar_gatos]:
+        Bd.nombre = ":memory:"
+        conexion = Bd.abrir()
+        try:
+            fn(conexion)
+            print(f"✅ {fn.__name__}")
+        except AssertionError as e:
+            print(f"❌ {fn.__name__}: {e}")
+        finally:
+            Bd.cerrar()
